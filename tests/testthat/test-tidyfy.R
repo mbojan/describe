59538357612d44891881837fc0b4cf7ae79b9a14
Michal Bojanowski <michal2992@gmail.com>
2016-04-28 12:03:42 +0200

    Testing tidyfy

--- tests/testthat/test-tidyfy.R
+++ tests/testthat/test-tidyfy.R
@@ -2,5 +2,8 @@ context("Tidyfy works on data frames")
 
 test_that("tidyfy works on data frames", {
   data(mtcars)
-  r <- tidyfy(mtcars)
+  mtcars$name <- rownames(mtcars)
+
+  r <- tidyfy(mtcars, by="cyl")
+  str(r)
 })
