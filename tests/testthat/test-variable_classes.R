a7ef75ee0b78ae7d25667bbe82f2fa85a32937f4
Michal Bojanowski <michal2992@gmail.com>
2016-04-28 12:54:36 +0200

    Split variable_classes tests

--- tests/testthat/test-variable_classes.R
+++ tests/testthat/test-variable_classes.R
@@ -1,27 +1,37 @@
 context("Test variable_classes")
 
-test_that("variable_classes works correctly on `mtcars`", {
-  # Add row names as a separate column
-  data(mtcars)
-  mtcars$name <- rownames(mtcars)
-  r <- variable_classes(mtcars)
+# Add row names as a separate column
+data(mtcars)
+mtcars$name <- rownames(mtcars)
+r <- variable_classes(mtcars)
+vnames <- names(mtcars)
 
-  # Returns a list
+
+
+
+test_that("variable_classes returns a proper list", {
   expect_type(r, "list")
 
   # Elements are vectors of variable names
   expect_true( all( sapply(r, class) == "character" ) )
+})
 
-  vnames <- names(mtcars)
+
+test_that("All variables in df can be found in the result", {
   l1 <- lapply(r, function(v) vnames %in% v )
   m <- do.call("rbind", l1)
+
   # All variables in df can be found in `r`
   expect_true( all(apply(m, 2, any)) )
 
   # ... and only once
   expect_true( all(colSums(m) == 1) )
+})
 
-  # All variable names in `r` appear in `df`
+
+
+test_that("All variable names in `r` appear in `df`", {
   l2 <- lapply(r, function(v) v %in% vnames )
   expect_true( all(sapply(l2, all))   )
+
 })
