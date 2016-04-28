618d6f1e50ee1e3f150d6c09aaf2fe4a3133d5f9
Michal Bojanowski <michal2992@gmail.com>
2016-04-28 16:20:49 +0200

    Added describe()

---
+++ R/describe.R
@@ -0,0 +1,30 @@
+#' Descriptive statistics
+#'
+#' @param x data frame
+#' @param ... other arguments passed to/from other methods
+#'
+#' @return Data frame with summaries
+#' @export
+#'
+#' @examples
+#' mtcars %>% group_by(cyl) %>% describe()
+#'
+describe <- function(x, ...) {
+  td <- tidyfy(x)
+  describe_numeric(td$numeric, ...)
+}
+
+# Describe tidy data frame of numeric variables
+describe_numeric <- function(x, ...) {
+  x %>%
+    summarise_(
+      m=lazyeval::interp(~mean(v), v=as.name("value")),
+      s=lazyeval::interp(~sd(v), v=as.name("value"))
+    ) %>%
+    ungroup() %>%
+    mutate( ch = paste0( round(m, 1), " (+-", round(s, 1), ")"))
+  # %>%
+  #   select_(cyl, key, ch) %>%
+  #   spread_(cyl, ch)
+}
+
